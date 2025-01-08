60d4309dfc2bea0fb8a958d99597c9e68e3bc032
leviackerman9000 <aeftiar@gmail.com>
2025-01-09 00:37:14 +0600

    added users class

--- users.py
+++ users.py
@@ -30,5 +30,25 @@ class Rider(User):
     def update_location(self, current_location):
         self.current_location = current_location
 
+    def request_ride(self, ride_sharing, destination):
+        pass
+
+    def show_current_ride(self):
+        print(self.current_ride)
+
+
+class Driver(User):
+    def __init__(self, name, email, nid, current_location):
+        super().__init__(name, email, nid)
+        self.current_location = current_location
+        self.wallet = 0
+
+    def display_profile(self):
+        print(f"Driver Name: {self.name}")
+
+    def accept_ride(self, ride):
+        # accept korbo
+        pass
+
     
 
